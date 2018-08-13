499c5544af2da8233e43082763b74b6baff62e51
Timothy Hill <tim@executivesuiteit.com>
2018-08-13 11:17:21 -0700

    Created the say view

---
+++ basic/views/site/say.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Description.
+ *
+ * @package ${namespace}
+ * @Since 1.0.0
+ * @author Timothy Hill
+ * @link https://executivesuiteit.com
+ * @license
+ */
+
+use yii\helpers\Html;
+?>
+<?= Html::encode($message) ?>
