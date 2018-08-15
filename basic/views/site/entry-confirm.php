46d914bc19e3ff637b119fdba8eaeb3afca4bf8f
Timothy Hill <tim@executivesuiteit.com>
2018-08-15 14:59:04 -0700

    added database models for Country code

---
+++ basic/views/site/entry-confirm.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description.
+ *
+ * @package ${namespace}
+ * @Since 1.0.0
+ * @author Timothy Hill
+ * @link https://executivesuiteit.com
+ * @license
+ */
+
+use yii\helpers\Html;
+?>
+<p>You have entered the following information:</p>
+
+<ul>
+	<li><label>Name</label>: <?= Html::encode($model->name) ?></li>
+	<li><label>Email</label>: <?= Html::encode($model->email) ?></li>
+</ul>
---
+++ basic/views/site/entry.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Description.
+ *
+ * @package ${namespace}
+ * @Since 1.0.0
+ * @author Timothy Hill
+ * @link https://executivesuiteit.com
+ * @license
+ */
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+?>
+<?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+
+<?php ActiveForm::end(); ?>
+
+
